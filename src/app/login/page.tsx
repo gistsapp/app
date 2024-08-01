102a88b170c52d9127ea691b725e2ca99107b9b6
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-08-01 20:26:10 +0200

    fix(GIST-15): login page responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 export default function Login() {
   return (
-    <div className="flex flex-col justify-center items-center gap-8 w-1/5 h-screen">
+    <div className="flex flex-col justify-center items-center gap-8 w-96 h-screen p-2">
       <h2>Log in to Gists</h2>
       <div className="flex flex-col gap-4 w-full">
         <Button>Log in with Email</Button>
