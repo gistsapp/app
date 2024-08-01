a7f30e0d01f36150f84098ecfb3634879a4ded35
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-08-01 20:23:23 +0200

    feat(GIST-15): create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+export default function Login() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 w-1/5 h-screen">
+      <h2>Log in to Gists</h2>
+      <div className="flex flex-col gap-4 w-full">
+        <Button>Log in with Email</Button>
+        <Button variant={'secondary'}>Log in with GitHub</Button>
+        <Button variant={'secondary'}>Log in with Google</Button>
+      </div>
+    </div>
+  )
+}
