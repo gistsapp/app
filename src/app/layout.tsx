faf83cc8f938f0d7524fd798f706164a2d21cfba
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-08-02 09:11:35 +0200

    fix(GIST-15): add theme path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import { Providers } from '../components/theme/theme-provider'
 import BlurBackground from '@/components/ui/blur-background'
 import ThemeWrapper from '@/components/theme/theme-wrapper'
 import { Toaster } from '@/components/shadcn/toaster'
+import { Providers } from '@/components/theme/theme-provider'
 
 const fontSans = FontSans({ subsets: ['latin'] })
 
