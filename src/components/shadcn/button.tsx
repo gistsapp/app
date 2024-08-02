e5146b9fcd9d6ebf7df931622dcc65fc329415ee
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-08-02 09:03:58 +0200

    refactor(GIST-15): create shadcn directory & add components

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -14,10 +14,11 @@ const buttonVariants = cva(
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'text-primary text-base underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-6 py-3',
+        'no-padding': 'h-10',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
