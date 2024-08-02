cdc5b7ee8d46fa7e003ab23d2fb0d79c4861a6e9
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-08-02 09:07:06 +0200

    refactor(GIST-15): update login page with different state

---
+++ src/components/ui/blur-background.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { useTheme } from 'next-themes'
+
+export default function BlurBackground() {
+  const { theme } = useTheme()
+
+  return (
+    <div className="fixed inset-0 -z-10 flex items-center justify-center pointer-events-none">
+      <div
+        className={cn(
+          'h-[600px] w-[600px] bg-primary opacity-30 rounded-full',
+          'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10',
+          theme === 'light' ? 'blur-[100px]' : 'blur-[300px]'
+        )}
+      />
+    </div>
+  )
+}
---
+++ src/pages/feature/login-feature.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import { useState, useCallback } from 'react'
+import { useForm, UseFormRegisterReturn } from 'react-hook-form'
+import Login from '../ui/login'
+import { useToast } from '@/components/shadcn/use-toast'
+
+interface FormData {
+  email: string
+}
+
+export default function LoginFeature() {
+  const [step, setStep] = useState<'initial' | 'emailInput' | 'otpInput'>('initial')
+  const [otpValue, setOtpValue] = useState('')
+  const { toast } = useToast()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    mode: 'onChange',
+  })
+
+  const emailRegister = register('email', {
+    required: 'Email is required',
+    pattern: {
+      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      message: 'Invalid email address',
+    },
+  })
+
+  const handleEmailClick = useCallback(() => {
+    if (step === 'initial') {
+      setStep('emailInput')
+    } else if (step === 'emailInput' && isValid) {
+      handleSubmit(onSubmit)()
+    }
+  }, [step, isValid, handleSubmit])
+
+  const handleGitHubClick = useCallback(() => {
+    console.log('GitHub')
+  }, [])
+
+  const handleGoogleClick = useCallback(() => {
+    console.log('CGoogle')
+  }, [])
+
+  const handleOtpChange = useCallback((value: string) => {
+    setOtpValue(value)
+  }, [])
+
+  const handleContinueClick = useCallback(() => {
+    console.log('OTP:', otpValue)
+  }, [otpValue])
+
+  const handleTryAgainClick = useCallback(() => {
+    console.log('A new OTP has been sent.')
+    toast({ title: 'A new one time password has been sent.', description: 'Please check your email.' })
+  }, [toast])
+
+  const handleBackToLoginClick = useCallback(() => {
+    setStep('initial')
+    setOtpValue('')
+  }, [])
+
+  const onSubmit = (data: FormData) => {
+    console.log(data)
+    setStep('otpInput')
+  }
+
+  return (
+    <Login
+      step={step}
+      email={emailRegister as UseFormRegisterReturn<'email'>}
+      otpValue={otpValue}
+      onEmailClick={handleEmailClick}
+      onGitHubClick={handleGitHubClick}
+      onGoogleClick={handleGoogleClick}
+      onOtpChange={handleOtpChange}
+      onContinueClick={handleContinueClick}
+      onTryAgainClick={handleTryAgainClick}
+      onBackToLoginClick={handleBackToLoginClick}
+      isEmailValid={isValid}
+      emailError={errors.email?.message}
+    />
+  )
+}
