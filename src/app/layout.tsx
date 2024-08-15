65e7e594e9e9101024bb69cdc56dc758019f6800
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2024-08-15 16:47:22 +0200

    feat(GIST-20): authentication
    * wip(GIST-20): implemented auth mechanism with google and github
    
    * feat(GIST-20): deployment + added redirections + fixed router structure
    
    * ops(GIST-20): env variable for front deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,39 @@
-import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
-import { cn } from '@/lib/utils'
-import BlurBackground from '@/components/ui/blur-background'
-import ThemeWrapper from '@/components/theme/theme-wrapper'
-import { Toaster } from '@/components/shadcn/toaster'
-import { Providers } from '@/components/theme/theme-provider'
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn, getBackendURL } from "@/lib/utils";
+import BlurBackground from "@/components/ui/blur-background";
+import ThemeWrapper from "@/components/theme/theme-wrapper";
+import { Toaster } from "@/components/shadcn/toaster";
+import { Providers } from "@/components/theme/theme-provider";
+import QueryProvider from "@/components/api/api-provider";
 
-const fontSans = FontSans({ subsets: ['latin'] })
+const fontSans = FontSans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Gists',
-  description: 'Create and share code snippets.',
-}
+  title: "Gists",
+  description: "Create and share code snippets.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
+  console.log(getBackendURL());
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={cn(fontSans.className)}>
-        <Providers>
-          <ThemeWrapper>
-            <div className="z-10 flex justify-center">{children}</div>
-            <BlurBackground />
-            <Toaster />
-          </ThemeWrapper>
-        </Providers>
+        <QueryProvider>
+          <Providers>
+            <ThemeWrapper>
+              <div className="z-10 flex justify-center">{children}</div>
+              <BlurBackground />
+              <Toaster />
+            </ThemeWrapper>
+          </Providers>
+        </QueryProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ src/lib/queries/auth.queries.tsx
@@ -0,0 +1,76 @@
+"use client";
+import ky from "ky";
+import { getBackendURL } from "../utils";
+import { useMutation } from "@tanstack/react-query";
+import getQueryClient from "./queries";
+
+const fetchLocalAuth = async ({ email }: { email: string }) => {
+  const json = await ky
+    .post(`${getBackendURL()}/auth/local/begin`, {
+      json: { email },
+    })
+    .json();
+
+  return json; //no need to assert type since the most important is the status code
+};
+
+const fetchLocalAuthVerify = async ({
+  email,
+  token,
+}: {
+  email: string;
+  token: string;
+}) => {
+  const json = await ky
+    .post(`${getBackendURL()}/auth/local/verify`, {
+      json: { email, token },
+      credentials: "include",
+    })
+    .json();
+  return json;
+};
+
+/**
+ * Start the local authentication process by sending an email to the user
+ * @param email, the user email
+ * @returns the mutation object, and error if any and a boolean indicating the loading state
+ */
+export const useLocalAuth = () => {
+  const { mutate, error, isPending } = useMutation({
+    mutationFn: (email: string) => {
+      return fetchLocalAuth({ email });
+    },
+  });
+  return { mutate, error, isPending };
+};
+
+export const prefetchLocalAuth = async (email: string) => {
+  const queryClient = getQueryClient();
+  return await queryClient.prefetchQuery({
+    queryKey: ["localAuth", email],
+    queryFn: () => fetchLocalAuth({ email }),
+  });
+};
+
+export const useLocalAuthVerify = () => {
+  const { mutate, error, isPending, data } = useMutation({
+    mutationFn: ({ email, token }: { email: string; token: string }) => {
+      return fetchLocalAuthVerify({ email, token });
+    },
+  });
+  return { mutate, error, isPending, data };
+};
+
+export const prefetchLocalAuthVerify = async ({
+  email,
+  token,
+}: {
+  email: string;
+  token: string;
+}) => {
+  const queryClient = getQueryClient();
+  return await queryClient.prefetchQuery({
+    queryKey: ["localAuthVerify", email, token],
+    queryFn: () => fetchLocalAuthVerify({ email, token }),
+  });
+};
