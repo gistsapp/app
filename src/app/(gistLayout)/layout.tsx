edca84d45d55a0122cc31f6cfc077a8ad43b3ea4
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-09-01 17:02:21 +0200

    wip(GIST-37): create and refetch a gist

--- src/app/(gistLayout)/layout.tsx
+++ src/app/(gistLayout)/layout.tsx
@@ -1,40 +1,59 @@
-'use client'
+"use client";
 
-import { ReactNode, useCallback } from 'react'
-import GistLayout from './layout-ui'
-import { useMe } from '@/lib/queries/user.queries'
-import { toast, useToast } from '@/components/shadcn/use-toast'
+import { ReactNode, useCallback } from "react";
+import GistLayout from "./layout-ui";
+import { useMe } from "@/lib/queries/user.queries";
+import { toast, useToast } from "@/components/shadcn/use-toast";
+import { useCreateGist } from "@/lib/queries/gists.queries";
 
-export default function GistLayoutFeature({ children }: { children: ReactNode }) {
-  const { data } = useMe()
-  const { toast } = useToast()
+export default function GistLayoutFeature({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { data } = useMe();
+  const { toast } = useToast();
+  const { mutate: createGist } = useCreateGist({
+    onSuccess: () => {
+      toast({
+        title: "Gist Created",
+        description: "Your gist has been created successfully",
+      });
+    },
+  });
 
   const onMyGistsClick = () => {
-    console.log('My Gists clicked')
-  }
+    console.log("My Gists clicked");
+  };
 
   const onCreateTeamClick = useCallback(
     (name: string) => {
-      console.log('Creating tea with name:', name)
+      console.log("Creating tea with name:", name);
       toast({
-        title: 'Team Created',
-        description: 'Your team has been created successfully',
-      })
+        title: "Team Created",
+        description: "Your team has been created successfully",
+      });
     },
-    [toast]
-  )
+    [toast],
+  );
 
   const onCreateGistClick = (name: string, content: string) => {
-    console.log(`Creating gist with name: ${name} \nand content: ${content}`)
-    toast({
-      title: 'Gist Created',
-      description: 'Your gist has been created successfully',
-    })
-  }
+    console.log(`Creating gist with name: ${name} \nand content: ${content}`);
+    createGist({
+      content,
+      name,
+    });
+  };
 
   return (
-    <GistLayout username={data?.name ?? ''} avatar={data?.picture ?? ''} onCreateTeamClick={onCreateTeamClick} onMyGistsClick={onMyGistsClick} onCreateGistClick={onCreateGistClick}>
+    <GistLayout
+      username={data?.name ?? ""}
+      avatar={data?.picture ?? ""}
+      onCreateTeamClick={onCreateTeamClick}
+      onMyGistsClick={onMyGistsClick}
+      onCreateGistClick={onCreateGistClick}
+    >
       {children}
     </GistLayout>
-  )
+  );
 }
