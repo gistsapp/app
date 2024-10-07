165276bb44233144d84d993d886ce443c16bf75e
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2024-10-07 08:45:36 +0200

    feat(GIST-41): added syntax highlighting (#12)
    * feat(GIST-41): added syntax highlighting
    
    * fix(GIST-41): deleted higlight js

---
+++ src/components/shadcn/codearea.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import { cn } from "@/lib/utils";
+
+export interface CodeareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+  language?: string;
+}
+
+const Codearea = ({ className, language, ...props }: CodeareaProps) => {
+  return (
+    <CodeEditor
+      value={props.value}
+      language={language}
+      onChange={props.onChange}
+      className={cn(
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className,
+      )}
+      style={{
+        backgroundColor: "#0C0D0E",
+        fontFamily:
+          "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+      }}
+    />
+  );
+};
+
+Codearea.displayName = "Codearea";
+
+export { Codearea };
---
+++ src/lib/language.ts
@@ -0,0 +1,118 @@
+interface Language {
+  name: string;
+  extension: string;
+}
+
+const languages: Language[] = [
+  { name: "abap", extension: "abap" },
+  { name: "aes", extension: "aes" },
+  { name: "apex", extension: "cls" },
+  { name: "azcli", extension: "azcli" },
+  { name: "bat", extension: "bat" },
+  { name: "bicep", extension: "bicep" },
+  { name: "brainfuck", extension: "b" },
+  { name: "c", extension: "c" },
+  { name: "cameligo", extension: "mligo" },
+  { name: "clike", extension: "clike" },
+  { name: "clojure", extension: "clj" },
+  { name: "coffeescript", extension: "coffee" },
+  { name: "cpp", extension: "cpp" },
+  { name: "csharp", extension: "cs" },
+  { name: "csp", extension: "csp" },
+  { name: "css", extension: "css" },
+  { name: "dart", extension: "dart" },
+  { name: "dockerfile", extension: "dockerfile" },
+  { name: "ecl", extension: "ecl" },
+  { name: "elixir", extension: "ex" },
+  { name: "erlang", extension: "erl" },
+  { name: "flow9", extension: "flow" },
+  { name: "freemarker2", extension: "ftl" },
+  { name: "fsharp", extension: "fs" },
+  { name: "go", extension: "go" },
+  { name: "graphql", extension: "graphql" },
+  { name: "handlebars", extension: "hbs" },
+  { name: "hcl", extension: "hcl" },
+  { name: "hcl", extension: "tf" },
+  { name: "html", extension: "html" },
+  { name: "ini", extension: "ini" },
+  { name: "java", extension: "java" },
+  { name: "javascript", extension: "js" },
+  { name: "json", extension: "json" },
+  { name: "jsx", extension: "jsx" },
+  { name: "julia", extension: "jl" },
+  { name: "kotlin", extension: "kt" },
+  { name: "less", extension: "less" },
+  { name: "lex", extension: "l" },
+  { name: "lexon", extension: "lex" },
+  { name: "liquid", extension: "liquid" },
+  { name: "livescript", extension: "ls" },
+  { name: "lua", extension: "lua" },
+  { name: "m3", extension: "m3" },
+  { name: "markdown", extension: "md" },
+  { name: "mips", extension: "s" },
+  { name: "msdax", extension: "dax" },
+  { name: "mysql", extension: "sql" },
+  { name: "nginx", extension: "nginx" },
+  { name: "objective-c", extension: "m" },
+  { name: "pascal", extension: "pas" },
+  { name: "pascaligo", extension: "ligo" },
+  { name: "perl", extension: "pl" },
+  { name: "pgsql", extension: "pgsql" },
+  { name: "php", extension: "php" },
+  { name: "pla", extension: "pla" },
+  { name: "plaintext", extension: "txt" },
+  { name: "postiats", extension: "dats" },
+  { name: "powerquery", extension: "pq" },
+  { name: "powershell", extension: "ps1" },
+  { name: "proto", extension: "proto" },
+  { name: "pug", extension: "pug" },
+  { name: "python", extension: "py" },
+  { name: "qsharp", extension: "qs" },
+  { name: "r", extension: "r" },
+  { name: "razor", extension: "cshtml" },
+  { name: "redis", extension: "redis" },
+  { name: "redshift", extension: "sql" },
+  { name: "restructuredtext", extension: "rst" },
+  { name: "ruby", extension: "rb" },
+  { name: "rust", extension: "rs" },
+  { name: "sb", extension: "sb" },
+  { name: "scala", extension: "scala" },
+  { name: "scheme", extension: "scm" },
+  { name: "scss", extension: "scss" },
+  { name: "shell", extension: "sh" },
+  { name: "sol", extension: "sol" },
+  { name: "sparql", extension: "sparql" },
+  { name: "sql", extension: "sql" },
+  { name: "st", extension: "st" },
+  { name: "stylus", extension: "styl" },
+  { name: "swift", extension: "swift" },
+  { name: "systemverilog", extension: "sv" },
+  { name: "tcl", extension: "tcl" },
+  { name: "toml", extension: "toml" },
+  { name: "tsx", extension: "tsx" },
+  { name: "twig", extension: "twig" },
+  { name: "typescript", extension: "ts" },
+  { name: "vb", extension: "vb" },
+  { name: "vbscript", extension: "vbs" },
+  { name: "verilog", extension: "v" },
+  { name: "vue", extension: "vue" },
+  { name: "xml", extension: "xml" },
+];
+
+function isDocker(file_name: string) {
+  return file_name === "Dockerfile";
+}
+
+function defaultStrategy(file_name: string) {
+  const extension = file_name.split(".").pop();
+  return (
+    languages.find((lang) => lang.extension === extension)?.name || "plaintext"
+  );
+}
+
+export function getLanguage(file_name: string) {
+  if (isDocker(file_name)) {
+    return "dockerfile";
+  }
+  return defaultStrategy(file_name);
+}
