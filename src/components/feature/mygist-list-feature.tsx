d73d5b76c104bc864760d4a95c6face7ebac9d01
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2024-10-08 19:34:19 +0200

    feat(GIST-53): delete gist on ui (#15)

--- src/components/feature/mygist-list-feature.tsx
+++ src/components/feature/mygist-list-feature.tsx
@@ -1,39 +1,45 @@
-'use client'
-import { Gist } from '@/types'
-import MyGistList from '../ui/mygist-list'
-import { useGists } from '@/lib/queries/gists.queries'
+"use client";
+import { Gist } from "@/types";
+import MyGistList from "../ui/mygist-list";
+import { useDeleteGist, useGists } from "@/lib/queries/gists.queries";
 
 // TODO: Get the gists list
 
 const gistMock: Gist[] = [
   {
-    id: '1',
-    name: 'My first Gist',
+    id: "1",
+    name: "My first Gist",
     code: 'console.log("Hello, World!")',
   },
   {
-    id: '2',
-    name: 'My second Gist',
+    id: "2",
+    name: "My second Gist",
     code: 'console.log("Hello, World!")',
   },
   {
-    id: '3',
-    name: 'My third Gist',
+    id: "3",
+    name: "My third Gist",
     code: 'console.log("Hello, World!")',
   },
   {
-    id: '4',
-    name: 'My fourth Gist',
+    id: "4",
+    name: "My fourth Gist",
     code: 'console.log("Hello, World!")',
   },
-]
+];
 
-const handleDeleteGist = (id: string) => {
-  console.log(`Deleting gist with ID: ${id}`)
-}
+const handleDeleteGist = (id: string) => {};
 
 export function MyGistListFeature() {
-  const { data } = useGists()
+  const { data } = useGists();
+  const { mutate: deleteGist } = useDeleteGist({
+    onSuccess: (id) => {
+      console.log(`Deleting gist with ID: ${id}`);
+    },
+  });
+  const handleDeleteGist = (id: string) => {
+    deleteGist(id);
+  };
 
-  return <MyGistList gists={data || []} onDeleteGist={handleDeleteGist} />
+  return <MyGistList gists={data || []} onDeleteGist={handleDeleteGist} />;
 }
