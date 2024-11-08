17fdb900347344bbbabcab8c1852098d1e6a05b1
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-11-08 14:01:07 +0100

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GistsLandingLogic from '@/components/logic/gists-landing-logic'
 
 export default function HomePage() {
   return (
-    <div className="w-full h-dvh sm:h-screen flex flex-row p-2">
+    <div className="w-full min-h-dvh sm:min-h-screen flex flex-row p-2">
       <script defer src="https://cloud.umami.is/script.js" data-website-id="0e9bcd71-c239-4666-9b8e-a7c9e99ae235"></script>
       <GistsLandingLogic />
     </div>
