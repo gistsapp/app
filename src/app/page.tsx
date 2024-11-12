3723895090d26e1bf601a0c02067a07a9218fb03
Rapidement <dorian.grasset.contact@gmail.com>
2024-11-12 20:11:38 +0100

    fix(umami): remove localhost analytics (#37)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import GistsLandingLogic from '@/components/logic/gists-landing-logic'
 export default function HomePage() {
   return (
     <div className="w-full min-h-dvh sm:min-h-screen flex flex-row p-2">
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="0e9bcd71-c239-4666-9b8e-a7c9e99ae235"></script>
       <GistsLandingLogic />
     </div>
   )
