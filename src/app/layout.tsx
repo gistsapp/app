287d51a3dae6b949e686eb74f8b78835e7e603d4
Rapidement <dorian.grasset.contact@gmail.com>
2024-11-15 15:55:37 +0100

    fix(umami): migrate to gists account (#46)
    * fix(umami): update data domains (#44)
    
    * chore: add license
    
    * fix(umami): migrate to gists account
    
    ---------
    
    Co-authored-by: tristan-mihai.radulescu <tristan-mihai.radulescu@etu.umontpellier.fr>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,8 +63,8 @@ export default function RootLayout({
       <Script
         defer
         src="https://cloud.umami.is/script.js"
-        data-website-id="0e9bcd71-c239-4666-9b8e-a7c9e99ae235"
-        data-domains="gits.app"
+        data-website-id="12b3d249-c64b-4990-8f21-2365da89f298"
+        data-domains="gists.app"
       />
       <body className={cn(fontSans.className)}>
         <QueryProvider>
